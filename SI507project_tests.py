882b954e2128c74e5d6e0dae363d71ebc4d40a7c
Luke <lukecui95@gmail.comm>
2019-04-17 22:01:16 -0400

    edit

--- SI507project_tests.py
+++ SI507project_tests.py
@@ -5,11 +5,30 @@ import os.path
 
 class National_parks_Tests(unittest.TestCase):
 
-
-
 	def setUp(self):
 		self.conn = sqlite3.connect("national_parks.sqlite") # Connecting to database that should exist
 		self.cur = self.conn.cursor()
+
+	#Route 1
+	def test_for_plot(self):
+		res = os.path.exists('static/state_plot.png')
+		self.assertTrue(res)
+
+	#Route 2
+	def test_num_of_parks(self):
+		state = 'AL'
+		self.assertEqual(state, 11)
+
+	#Route 3
+	def info_of_park(self):
+		self.cur.execute("select Type, Location, Challenge_states from Parks where Name = Evergreen")
+		data = self.cur.fetchone()
+		self.assertEqual(data,('National Park', 'FL', 'FL'), "Testing data that results from selecting Park 1")
+
+	#Route 4
+	def test_advice(self):
+		pass
+	#Not figure out how to test this route yet.
 
 	def test_for_national_park_table(self):
 		self.cur.execute("select Name, Type, Location, Challenge_states from Parks where ID = 1")
@@ -31,29 +50,7 @@ class National_parks_Tests(unittest.TestCase):
 		data = self.cur.fetchone()
 		self.assertEqual(data, state, "Testing a select state where Id = 1")
 
-    #Route 1
-    def test_for_plot(self):
-        res = os.path.exists('static/state_plot.png')
-        self.assertTrue(res)
-
-    #Route 2
-    def test_num_of_parks(self):
-        state = 'AL'
-        self.assertEqual(state, 11)
-
-    #Route 3
-    def info_of_park(self):
-        self.cur.execute("select Type, Location, Challenge_states from Parks where Name = Evergreen")
-		data = self.cur.fetchone()
-		self.assertEqual(data,('National Park', 'FL', 'FL'), "Testing data that results from selecting Park 1")
-
-
-    #Route 4
-    def test_advice(self):
-		pass
-        ###Not figure out how to test this route yet.
-
-    def tearDown(self):
+	def tearDown(self):
 		self.conn.commit()
 		self.conn.close()
 
